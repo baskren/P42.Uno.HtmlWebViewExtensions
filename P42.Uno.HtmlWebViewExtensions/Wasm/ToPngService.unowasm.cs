050d3b401df6f0054d25b86a83c3567286615dd6
Ben Askren <ben@buildcalc.com>
2021-01-29 14:18:59 -0500

    Added Demo projects

---
+++ P42.Uno.HtmlWebViewExtensions/Wasm/ToPngService.unowasm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Uno.Foundation;
+using Uno.UI;
+using Windows.UI.Xaml;
+
+namespace P42.Uno.HtmlWebViewExtensions
+{
+    class NativeToPngService : INativeToPngService
+    {
+        public bool IsAvailable => true;
+
+        public Task<ToFileResult> ToPngAsync(string html, string fileName, int width)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ToFileResult> ToPngAsync(WebView webView, string fileName, int width)
+        {
+            var id = webView.GetHtmlAttribute("id");
+            var result = await WebAssemblyRuntime.InvokeAsync($"UnoScreenshot_GetUrlPromise('{id}', {width})");
+            Console.WriteLine("PlatformCaptureAsync result:" + result);
+            if (result.StartsWith("success: true"))
+            {
+
+            }
+        }
+    }
+}
---
+++ P42.Uno.HtmlWebViewExtensions/WasmScripts/UnoPng.js
@@ -0,0 +1,38 @@
+﻿require([`${config.uno_app_base}/html2canvas.min`], c => window.html2canvas = c);
+
+function UnoPng_GetUrlPromise(id, width) {
+    return new Promise(function (resolve, reject) {
+        //var canvasResult = html2canvas(document.body);
+        let element = document.body;
+        if (id !== undefined && typeof id !== 'undefined') {
+            element = document.getElementById(id);
+        }
+        let options = new Object();
+        if (width !== undefined && typeof id !== 'undefined') {
+            options.width = width;
+        }
+        var canvasResult = html2canvas(element, options);
+
+        canvasResult.then(function (canvas) {
+
+            let jepgPath = UnoPng_CreateBase64Image(canvas, "image/jpeg");
+            let pngPath = UnoPng_CreateBase64Image(canvas, "image/png");
+            resolve('success: true, Width: ' + canvas.width + ', Height: ' + canvas.height + ', PngPath: ' + pngPath + ", JpegPath: " + jepgPath);
+        });
+
+        canvasResult.catch(function (reason) {
+            console.log('E.1');
+            reject('success: false, reason: ' + reason);
+        });
+
+    });
+}
+
+function UnoPng_CreateBase64Image(canvas, mimeType) {
+    let urlImage = canvas.toDataURL(mimeType);
+    let base64Image = urlImage.replace("data:" + mimeType + ";base64,", "");
+    let suffix = mimeType.replace("image/", "");
+    let path = '/tmp/' + uuidv4() + "." + suffix;
+    FS.writeFile(path, base64Image);
+    return path;
+}
